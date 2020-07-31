a8e0ea8562a5bee88841544dd0a8a2588d280b6f
Harm Geerts <hgeerts@osso.nl>
2020-07-31 13:20:24 +0200

    Simplify cachebuster with single unique string

--- cachebuster/templatetags/cachebuster.py
+++ cachebuster/templatetags/cachebuster.py
@@ -1,12 +1,9 @@
 __author__ = 'James Addison'
 
-import posixpath
-import datetime
-import urllib
-import os
-
 from django import template
 from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.core.files.storage import default_storage
 
 try:
     # finders won't exist if we're not using Django 1.3+
@@ -38,7 +35,7 @@ class CacheBusterTag(template.Node):
             raise template.TemplateSyntaxError("'%r' tag must have one or two arguments" % token.contents.split()[0])
 
         self.path = tokens[1]
-        self.force_timestamp = len(tokens) == 3 and tokens[2] or False
+        self.unique_string = getattr(settings, 'CACHEBUSTER_UNIQUE_STRING', 'no_cachebuster_set')
 
     def render(self, context):
         # self.path may be a template variable rather than a simple static file string
@@ -47,41 +44,7 @@ class CacheBusterTag(template.Node):
         except template.VariableDoesNotExist:
             path = self.path
 
-        path = posixpath.normpath(path).lstrip('/')
-
         if self.is_media:
-            url_prepend = settings.MEDIA_URL
-            unique_prepend = getattr(settings, 'CACHEBUSTER_PREPEND_MEDIA', False)
-            unique_string = self.get_file_modified(os.path.join(settings.MEDIA_ROOT, path))
+            return default_storage.url(path) + '?' + self.unique_string
         else:
-            # django versions < 1.3 don't have a STATIC_URL, so fall back to MEDIA_URL
-            url_prepend = getattr(settings, "STATIC_URL", settings.MEDIA_URL)
-            unique_prepend = getattr(settings, 'CACHEBUSTER_PREPEND_STATIC', False)
-
-            if settings.DEBUG and finders:
-                absolute_path = finders.find(path)
-            else:
-                # django versions < 1.3 don't have a STATIC_ROOT, so fall back to MEDIA_ROOT
-                absolute_path = os.path.join(getattr(settings, 'STATIC_ROOT', settings.MEDIA_ROOT), path)
-
-            if self.force_timestamp:
-                unique_string = self.get_file_modified(absolute_path)
-            else:
-                unique_string = getattr(settings, 'CACHEBUSTER_UNIQUE_STRING', None)
-                if not unique_string:
-                    unique_string = self.get_file_modified(absolute_path)
-
-        # Add in harder cachebusting required for CloudFront et al
-        if unique_prepend:
-          return url_prepend + unique_string + '/' + path
-        else:
-          return url_prepend + path + '?' + unique_string
-
-    def get_file_modified(self, path):
-        try:
-            return datetime.datetime.fromtimestamp(os.path.getmtime(os.path.abspath(path))).strftime('%S%M%H%d%m%y')
-        except:
-            # if the file can't be found, return this; it will be an
-            # indicator to the developer that collectstatic needs to be run
-            # (as if the resulting 404 for the missing file wouldn't be)
-            return '000000000000'
+            return staticfiles_storage.url(path) + '?' + self.unique_string
